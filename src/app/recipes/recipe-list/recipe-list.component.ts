2d0f400247570855f511dac906efa2120ad4a46d
acacia1021 <acacia1021@gmail.com>
2022-12-29 14:23:56 -0500

    Adding content to the Recipes Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,13 @@ import { Component } from '@angular/core';
 })
 export class RecipeListComponent {
 
-  recipes = [];
+  // recipes will be of Recipe type, as an array == to an empty array at first, and then reference the model pieces
+  // Here, the pieces are name, description, and imagePath
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a Test', 'https://th.bing.com/th/id/OIP.9j6H7W8MRTMeZBv_KfSnIQHaJQ?pid=ImgDet&rs=1')
+  ];
+
+
 
   
 
