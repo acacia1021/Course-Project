815297819de50fb710488433c7534d3448394095
Lord, Acacia [USA] <Lord_Acacia@bah.com>
2023-04-11 15:28:52 -0400

    input fields for shopping list - local references

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+
+  @ViewChild('nameInput', { static: false }) nameInputReference: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputReference: ElementRef;
+
+  // // Passing in an object with the following elements
+  //   ingredientAdded = new EventEmitter<{name: string, amount: number}>();
+
+  // We have an Ingredient model to do the same as the example of the object above
+ @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+
+  onAddItem() {
+    const ingredientName = this.nameInputReference.nativeElement.value;
+    const ingredientAmount = this.amountInputReference.nativeElement.value;
+    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
 
 }
