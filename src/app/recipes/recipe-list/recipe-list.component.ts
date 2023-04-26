aaf9c933455fc9ff4c655495de9e69eef85a0d41
Lord, Acacia [USA] <Lord_Acacia@bah.com>
2023-04-26 12:55:34 -0400

    Managing recipes in the RecipeService

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,33 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
 
   // recipes will be of Recipe type, as an array == to an empty array at first, and then reference the model pieces
   // Here, the pieces are name, description, and imagePath
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a Test', 'https://th.bing.com/th/id/OIP.9j6H7W8MRTMeZBv_KfSnIQHaJQ?pid=ImgDet&rs=1'),
-    new Recipe('A Second Test Recipe', 'This is simply a second Test', 'https://th.bing.com/th/id/OIP.9j6H7W8MRTMeZBv_KfSnIQHaJQ?pid=ImgDet&rs=1')
-  ];
+
+  //Moved to Recipe.service
+  // recipes: Recipe[] = [
+  //   new Recipe('A Test Recipe', 'This is simply a Test', 'https://th.bing.com/th/id/OIP.9j6H7W8MRTMeZBv_KfSnIQHaJQ?pid=ImgDet&rs=1'),
+  //   new Recipe('A Second Test Recipe', 'This is simply a second Test', 'https://th.bing.com/th/id/OIP.9j6H7W8MRTMeZBv_KfSnIQHaJQ?pid=ImgDet&rs=1')
+  // ];
+
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService) {
+
+  }
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
+
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,13 @@
-export class RecipeService {
+import { Recipe } from "./recipe.model";
 
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a Test', 'https://th.bing.com/th/id/OIP.9j6H7W8MRTMeZBv_KfSnIQHaJQ?pid=ImgDet&rs=1'),
+        new Recipe('A Second Test Recipe', 'This is simply a second Test', 'https://th.bing.com/th/id/OIP.9j6H7W8MRTMeZBv_KfSnIQHaJQ?pid=ImgDet&rs=1')
+    ];
+
+    getRecipes() {
+        //Will return a new instance of the same array
+        return this.recipes.slice();
+    }
 }
