cab2625b5cecc4b95be41d79bba82ef5223322bb
Lord, Acacia [USA] <Lord_Acacia@bah.com>
2023-04-28 11:31:36 -0400

    Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,11 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a Test', 'https://th.bing.com/th/id/OIP.9j6H7W8MRTMeZBv_KfSnIQHaJQ?pid=ImgDet&rs=1'),
-        new Recipe('A Second Test Recipe', 'This is simply a second Test', 'https://th.bing.com/th/id/OIP.9j6H7W8MRTMeZBv_KfSnIQHaJQ?pid=ImgDet&rs=1')
+        new Recipe('Schnitzel', 'Delicious German Recipe', 'https://thumbs.dreamstime.com/b/schnitzel-french-fries-dish-served-ketchup-mayonnaise-lemon-vegetables-close-up-white-plate-87430745.jpg', [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
+        new Recipe('Burger', 'An American Classic', 'https://i0.wp.com/thenutritionadventure.com/wp-content/uploads/2017/07/PourHouseAmericanBurger.jpg?resize=5236%2C3490', [new Ingredient('Buns', 2), new Ingredient('Meat', 1)])
     ];
 
     recipeSelected = new EventEmitter<Recipe>();
