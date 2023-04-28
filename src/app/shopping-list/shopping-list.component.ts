383792414a6dcaabfae8d1eb6f882da2f61a2a2b
Lord, Acacia [USA] <Lord_Acacia@bah.com>
2023-04-28 11:17:06 -0400

    Fixed ShoppingList Service - pushing from A to B

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -22,6 +22,11 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    );
   }
 
 
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,4 @@
+import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
@@ -7,11 +8,18 @@ export class ShoppingListService {
         new Ingredient('Tomatoes', 10)
     ];
 
+    //Allows for the copy of the Ingredients to be pushed in the second line of addIngredient()
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
     getIngredients() {
+        //.slice() creates a copy of the original array - which is then used in addIngredient()
         return this.ingredients.slice();
     }
 
     addIngredient(ingredient: Ingredient) {
+        //Does not push to the UI, only updates in logic, because it is a NEW array
         this.ingredients.push(ingredient);
+        //Pushed to UI
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
